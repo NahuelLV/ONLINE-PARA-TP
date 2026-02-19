4bb36bccebc8f76f057e17c0f31a6d1fe37afd5f
SantinoGiuffrida <130598051+Santino-Giuffrida-Castillo@users.noreply.github.com>
2026-02-19 15:18:54 -0300

    Add files via upload

---
+++ lwjgl3/src/main/java/pro/juego/Ironfall/lwjgl3/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package pro.juego.Ironfall.lwjgl3;
+import pro.juego.Ironfall.IronfallJuego;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+
+    public static void main(String[] arg) {
+
+        Lwjgl3ApplicationConfiguration config =
+                new Lwjgl3ApplicationConfiguration();
+
+        config.setTitle("Ironfall");
+        config.setWindowedMode(1280, 720);
+        config.useVsync(true);
+
+        new Lwjgl3Application(new IronfallJuego(), config);
+    }
+}
